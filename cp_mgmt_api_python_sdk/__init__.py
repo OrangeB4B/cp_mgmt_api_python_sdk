5fe151dbe935aa780a7c7ed38fda15b8776cfa66
Pablo Muñoz <pablo.munoz@xbyorange.com>
2018-10-10 16:30:44 +0200

    Build package with 2.7 instead of 3

--- cp_mgmt_api_python_sdk/__init__.py
+++ cp_mgmt_api_python_sdk/__init__.py
@@ -1,4 +1,1 @@
-from mgmt_api import APIClient
-from mgmt_api import APIClientArgs
-from api_exceptions import APIException
-from api_exceptions import APIClientException
+name = "cp_mgmt_api_python_sdk"
